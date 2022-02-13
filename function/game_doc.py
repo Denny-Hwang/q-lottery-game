016a1e67808abe4c99389387920d3ba34178f04d
Sungjoo(Dennis) Hwang <48212469+Denny-Hwang@users.noreply.github.com>
2022-02-14 00:25:56 +0900

    Add files via upload

---
+++ function/game_doc.py
@@ -0,0 +1,29 @@
+import streamlit as st
+
+def lotto_doc():
+    st.image("https://www.dhlottery.co.kr/images/company/img_bi_intro_logo1.png", width=300)
+    st.title("Korean lottery - *Lotto 6/45*")
+    st.write(
+        """
+        In the **Lotto 6/45**, you can choose six numbers in the 1~45 range
+        """)
+
+def powerball_doc():
+    st.image(
+        "https://s3.amazonaws.com/cdn.powerball.com/drupal/files/2020-02/Powerball%20%2B%20PP%20Horizontal-Flat-Color.jpg",
+        width=400)
+    st.title("USA lottery - *Power ball*")
+
+    st.write(
+        """       
+        In the **Power ball**, you can choose five *white ball* in the 1~69 range and select one *power ball* in the 1~26 range
+        """)
+
+def custom_doc():
+    st.title("Custom Q-Lottery Game")
+
+    st.write(
+        """       
+        Customize your Q-Lottery Game
+
+        """)
---
+++ function/q_function.py
@@ -0,0 +1,114 @@
+import numpy as np
+from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit
+from qiskit import Aer, execute
+from qiskit.visualization import *
+from tqdm import tqdm
+
+def random_number(bits=6):
+    qr = QuantumRegister(bits, 'qubit')
+    cr = ClassicalRegister(bits, 'c_bit')
+    circuit = QuantumCircuit(qr,cr)
+    circuit.reset(range(bits))
+    circuit.h(qr)
+
+    circuit.measure_all()
+
+    backend = Aer.get_backend('qasm_simulator')
+    result = execute(circuit, backend, shots=1, memory=True).result()
+    counts = result.get_memory()
+    num = counts[0].split(" ")[0]
+    circuit_fig = circuit_drawer(circuit, output='mpl',
+                                 scale=1, vertical_compression='high',
+                                 style={'backgroundcolor': '#EEEEEE'})
+
+    return circuit_fig, counts[0], int(num, 2)
+
+def random_number_with_birthday(month, day, bits=6):
+    qr = QuantumRegister(bits, 'qubit')
+    q_birth = QuantumRegister(1, 'q-month')
+    q_day = QuantumRegister(1, 'q-day')
+    cr = ClassicalRegister(bits)
+
+    circuit = QuantumCircuit(qr, q_birth, q_day, cr)
+    circuit.reset(range(bits + 2))
+    circuit.h(qr)
+
+    ## birth-day initialization
+    month_init = month / 12
+    day_30 = [4, 6, 9, 11]
+    if month == 2:
+        if day == 29:
+            day_init = day / 29
+        else:
+            day_init = day / 28
+    elif np.isin(month, day_30):
+        day_init = day / 30
+    else:
+        day_init = day / 31
+
+    print(month_init, day_init)
+    circuit.rx(month_init, bits)
+    circuit.rx(day_init, bits + 1)
+
+    circuit.barrier()
+
+    circuit.cx(bits, month % bits)
+    circuit.cx(bits + 1, day % bits)
+
+    circuit.barrier()
+
+    circuit.measure(qr, cr)
+
+    backend = Aer.get_backend('qasm_simulator')
+    result = execute(circuit, backend, shots=1, memory=True).result()
+    counts = result.get_memory()
+    num = counts[0].split(" ")[0]
+    circuit_fig = circuit_drawer(circuit, output='mpl',
+                                 scale=1, vertical_compression='high',
+                                 style={'backgroundcolor': '#EEEEEE'})
+
+    return circuit_fig, counts[0], int(num, 2)
+
+def q_rng_lotto(bits=6, upper_bound=45):
+    circuit_fig, raw_bits, decimal = random_number(bits=bits)
+
+    while ((decimal==0) | (decimal > upper_bound)):
+        circuit_fig, raw_bits, decimal = random_number(bits=bits)
+
+    return circuit_fig, raw_bits, decimal
+
+def q_rng_lotto_with_birthday(month, day, bits=6, upper_bound=45):
+    circuit_fig, raw_bits, decimal = random_number_with_birthday(month, day, bits=bits)
+
+    while ((decimal == 0) | (decimal > upper_bound)):
+        circuit_fig, raw_bits, decimal = random_number_with_birthday(month, day, bits=bits)
+
+    return circuit_fig, raw_bits, decimal
+
+def get_rng_lotto(n_get_num = 6, bits=6, upper_bound=45):
+    lotto = []
+    for i in tqdm(range(n_get_num)):
+        _, _, decimal = q_rng_lotto(bits=bits, upper_bound=upper_bound)
+
+        # If the generated number is already in the list, regenerate the number to avoid duplication.
+        while (np.isin(decimal, lotto) == 1):
+            _, _, decimal = q_rng_lotto(bits=bits, upper_bound=upper_bound)
+
+        lotto.append(decimal)
+        lotto.sort()
+
+    return lotto
+
+def get_rng_lotto_with_birthday(month, day, n_get_num = 6, bits=6, upper_bound=45):
+    lotto = []
+    for i in tqdm(range(n_get_num)):
+        _, _, decimal = q_rng_lotto_with_birthday(month, day, bits=bits, upper_bound=upper_bound)
+
+        # If the generated number is already in the list, regenerate the number to avoid duplication.
+        while (np.isin(decimal, lotto) == 1):
+            _, _, decimal = q_rng_lotto_with_birthday(month, day, bits=bits, upper_bound=upper_bound)
+
+        lotto.append(decimal)
+        lotto.sort()
+
+    return lotto
